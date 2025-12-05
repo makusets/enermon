f4a1722cf9b9cbc9bc414bdae00acb97d63bdb54
makusets <68693442+makusets@users.noreply.github.com>
2025-12-05 17:06:23 +1100

    u

--- components/enermon/sensor.py
+++ components/enermon/sensor.py
@@ -89,44 +89,37 @@ async def to_code(config):
         if i < len(ct_names) and ct_names[i]:
             name_base = ct_names[i]
 
-        current = await sensor.new_sensor(config)  # <-- add await
-        current.set_parent(var)
+        current = cg.new_Pvariable(cg.new_variable(sensor.Sensor))  # <-- create Sensor directly
         current.set_name(f"{name_base} Current RMS")
         current.set_unit_of_measurement("A")
         cg.add(var.sensor_current_rms[i].assign(current))
 
-        power = await sensor.new_sensor(config)  # <-- add await
-        power.set_parent(var)
+        power = cg.new_Pvariable(cg.new_variable(sensor.Sensor))
         power.set_name(f"{name_base} Active Power")
         power.set_unit_of_measurement("W")
         cg.add(var.sensor_power_w[i].assign(power))
 
-        ed = await sensor.new_sensor(config)  # <-- add await
-        ed.set_parent(var)
+        ed = cg.new_Pvariable(cg.new_variable(sensor.Sensor))
         ed.set_name(f"{name_base} Energy Today")
         ed.set_unit_of_measurement("Wh")
         cg.add(var.sensor_energy_daily_wh[i].assign(ed))
 
-        ew = await sensor.new_sensor(config)  # <-- add await
-        ew.set_parent(var)
+        ew = cg.new_Pvariable(cg.new_variable(sensor.Sensor))
         ew.set_name(f"{name_base} Energy Week")
         ew.set_unit_of_measurement("Wh")
         cg.add(var.sensor_energy_weekly_wh[i].assign(ew))
 
-        em = await sensor.new_sensor(config)  # <-- add await
-        em.set_parent(var)
+        em = cg.new_Pvariable(cg.new_variable(sensor.Sensor))
         em.set_name(f"{name_base} Energy Month")
         em.set_unit_of_measurement("Wh")
         cg.add(var.sensor_energy_monthly_wh[i].assign(em))
 
-    voltage = await sensor.new_sensor(config)  # <-- add await
-    voltage.set_parent(var)
+    voltage = cg.new_Pvariable(cg.new_variable(sensor.Sensor))
     voltage.set_name("Line Voltage RMS")
     voltage.set_unit_of_measurement("V")
     cg.add(var.sensor_voltage_rms.assign(voltage))
 
-    wifi = await sensor.new_sensor(config)  # <-- add await
-    wifi.set_parent(var)
+    wifi = cg.new_Pvariable(cg.new_variable(sensor.Sensor))
     wifi.set_name("WiFi RSSI")
     wifi.set_unit_of_measurement("dBm")
     cg.add(var.sensor_wifi_rssi.assign(wifi))
